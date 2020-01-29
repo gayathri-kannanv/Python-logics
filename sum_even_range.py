deac33916ba7621f2d11ac91f899f05ae831d808
GAYATHRI K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-01-29 15:44:31 +0530

    Create sum_even_range.py

---
+++ sum_even_range.py
@@ -0,0 +1,8 @@
+min=int(input("ENTER THE MINIMUM VALUE OF THE RANGE:"))
+max=int(input("ENTER THE MAXIMUM VALUE OF THE RANGE:"))
+sum=0
+for i in range (min,max):
+  if i%2==0:        
+    sum=sum+i
+print("THE SUM OF PRIME NUMBERS IN THE GIVEN RANGE IS: ",sum)
+
