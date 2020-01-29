89df04fdd77356c2704553c9bac1cee06c9a2814
GAYATHRI K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-01-29 15:52:16 +0530

    Create sum_two_num.py

---
+++ sum_two_num.py
@@ -0,0 +1,8 @@
+ch=”y”
+while(ch=="yes" or ch=="y"):
+  a=int(input("enter first number:"))
+  b= int(input("enter second number"))
+  c=a+b
+  print("sum=",c)
+  print("do u want to continue:")
+  ch=input()
