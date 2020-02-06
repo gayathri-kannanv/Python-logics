0e6c980707574cb9217b35cc8d9634aa26b933d4
GAYATHRI K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-02-06 19:54:49 +0530

    Create print_logic.py

---
+++ print_logic.py
@@ -0,0 +1,13 @@
+x = 1
+print(++++x) 
+
+Options:
+1)2
+2)1
+3)3
+4)4
+
+Answer:2) 1
+
+Reason: Here, the loop is not used. Hence if we print a variable inside print,
+        the value is printed. '+' symbol doesn't matter in a print statement.
