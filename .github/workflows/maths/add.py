224938e2912b4b270c4be759b5c2d3c1153670c5
GAYATHRI K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-02-20 20:57:05 +0530

    Add files via upload

---
+++ .github/workflows/maths/add.py
@@ -0,0 +1,5 @@
+def add():
+    a=int(input("ENTER 1ST VALUE  :  "))
+    b=int(input("ENTER 2nd VALUE  :  "))
+    c=a+b
+    return c
---
+++ .github/workflows/maths/mul.py
@@ -0,0 +1,6 @@
+def mul():
+    a=int(input("ENTER 1ST VALUE  :  "))
+    b=int(input("ENTER 2nd VALUE  :  "))
+    c=a*b
+    return c
+    
