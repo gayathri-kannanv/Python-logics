045fad8e1480d28577707bc04ed43c9a5079016c
GAYATHRI K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-03-21 18:54:44 +0530

    Add files via upload

---
+++ Emoji convertor.py
@@ -0,0 +1,14 @@
+msg=input(">")
+words=msg.split(" ")
+
+emojis={
+    ":)":"😊",
+    ":(":"🙁"
+    }
+output=""
+for word in words:
+    output+=emojis.get(word, word)+ " "
+print(output)
+    
+     
+   
