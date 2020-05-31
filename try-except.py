29e3ac8f3463127a916eebacf7b1ca965bed1c4a
GAYATHRI K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-05-31 12:32:51 +0530

    Update try-except.py

--- try-except.py
+++ try-except.py
@@ -8,3 +8,19 @@ if(n>=0):
     print("It's a valid number")
 else:
     print("Not a number")
+
+    
+    
+    
+    
+       (OR)
+
+num=input("ENTER A NUMBER: ")
+try:
+    n=int(num)
+except:
+    print("Not a number")
+    quit()
+print("It's a valid number")
+
+   
